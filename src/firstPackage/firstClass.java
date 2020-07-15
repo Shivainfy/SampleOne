c411ae5b76c048b278d2b265e8f2db332c1376af
Shivainfy <shivabadiger7@gmail.com>
2020-07-15 07:32:29 -0230

    added new code to old repository

---
+++ src/firstPackage/firstClass.java
@@ -0,0 +1,12 @@
+package firstPackage;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		String name="Shiva";
+		int num=545;
+		System.out.println(name+ " " + num);
+		
+	}
+
+}
