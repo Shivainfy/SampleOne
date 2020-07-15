a5c7de7dfce922652deb1b7477fc6cbc20c2efe5
Shivainfy <42602719+Shivainfy@users.noreply.github.com>
2020-07-15 07:35:01 -0230

    Update firstClass.java

--- src/firstPackage/firstClass.java
+++ src/firstPackage/firstClass.java
@@ -1,11 +1,18 @@
 package firstPackage;
 
 public class firstClass {
-
+	
 	public static void main(String[] args) {
 		String name="Shiva";
 		int num=545;
 		System.out.println(name+ " " + num);
+		if(name="Shiva")
+		{
+			System.out.println("correct");
+		}
+		else{
+			System.out.println("not correct");
+		}
 		
 	}
 
